ad056d6133433db520f4557b5457496ab5a6f7df
Duc Le <45700189+dukele44@users.noreply.github.com>
2018-12-15 17:38:17 -0500

    Update Farebox.R

--- Farebox.R
+++ Farebox.R
@@ -6,7 +6,12 @@ library(odbc)
 
 con <- dbConnect(RSQLite::SQLite(), "mydb.db")
 
-
-dbReadTable(con, "FareBox")
+#1
+system <- dbGetQuery(con, "SELECT System FROM FareBox")
+continent <- dbGetQuery(con, "SELECT Continent FROM FareBox")
+country <- dbGetQuery(con, "SELECT Country FROM FareBox")
+ratio <- dbGetQuery(con, "SELECT Farebox_Ratio FROM FareBox")
+fare_system <- dbGetQuery(con, "SELECT Fare_System FROM FareBox")
+rate <- dbGetQuery(con, "SELECT Fare_Rate_USD FROM FareBox")
 
 dbDisconnect(con)
